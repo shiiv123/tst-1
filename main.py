5969b2e5e75293d70681b1fecdbe65695173a9b6
Shiv Bhushan Bhushan <69459822+shiiv123@users.noreply.github.com>
2023-03-17 19:07:21 +0000

    added button css, added jobs list in python flask and connected to html

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+Jobs = [
+  {'id': 1,
+   'title': 'data anlyst',
+   'location': 'bangalore'
+  },
+   {'id': 2,
+   'title': 'data scientist',
+   'location': 'pune'
+  },
+ {'id': 3,
+   'title': 'financial anlyst',
+   'location': 'delhi'
+  }
+]
 
 @app.route("/")
 def hello_world():
  
-  return render_template('home.html')
+  return render_template('home.html',jobs =Jobs, company = 'shiv')
 
 if __name__ == "__main__" :
   app.run(host='0.0.0.0', debug=True)
