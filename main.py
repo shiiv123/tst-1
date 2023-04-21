d1cf0903a02ff028887628e0bac48a7730fe225f
Shiv Bhushan Bhushan <69459822+shiiv123@users.noreply.github.com>
2023-04-21 18:11:35 +0000

    new temp

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-Jobs = [
-  {'id': 1,
-   'title': 'data anlyst',
-   'location': 'bangalore',
-   'salary': 120000
-  },
-   {'id': 2,
-   'title': 'data scientist',
-   'location': 'pune'
-  },
- {'id': 3,
-   'title': 'financial anlyst',
-   'location': 'delhi'
-  }
-]
 
-@app.route("/")
-def hello_world():
- 
-  return render_template('home.html',jobs =Jobs, company = 'shiv')
+@app.route("/hello")
+def index():
+  return render_template('home.html')
 
 if __name__ == "__main__" :
   app.run(host='0.0.0.0', debug=True)
