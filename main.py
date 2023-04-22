aea004d7761a61b09c2d657b0c9cf9c084948573
shiiv123 <69459822+shiiv123@users.noreply.github.com>
2023-04-22 18:28:05 +0000

    Made first app

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 
 app = Flask(__name__)
+app.secret_key = "1234"
 
-@app.route("/hello")
+@app.route("/")
 def index():
+  flash("what's your name?")
+  return render_template('home.html')
+  
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+  flash("Hi " + str(request.form['name_input']) + ", greet to see it")
   return render_template('home.html')
 
 if __name__ == "__main__" :
